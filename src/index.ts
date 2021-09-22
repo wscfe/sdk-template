7fad9c5777b4c3262d3065d052e00f8db4e6dba5
王事成 <wangshicheng@bytedance.com>
2021-09-22 13:43:04 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: wangshicheng
+ * @Date: 2021-09-11 13:21:35
+ * @Description: SDK入口文件
+ * @FilePath: /clip-img/src/index.ts
+ */
+
+export default class TsSdk {
+  /**
+   *
+   * @param {number} a
+   * @param {number} b
+   * @returns {number} sum
+   */
+  static add(a: number, b: number): number {
+    return a + b
+  }
+}
